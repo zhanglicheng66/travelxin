a44f9d619c699844a39513c989b9a158bf701e5e
zlc <405328994@qq.com>
2019-07-14 22:41:32 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="wrapper">
+     <swiper :options="swiperOption" >
+    <!-- slides -->
+    <swiper-slide v-for="item in swiperList" :key='item.id'>
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+    </swiper-slide>
+    
+  
+    <!-- Optional controls -->
+    <div class="swiper-pagination"  slot="pagination"></div>
+
+  </swiper>
+</div>
+   
+</template>
+
+<script>
+    export default {
+        name:'HomeSwiper',
+        data(){
+            return{
+                swiperOption:{
+                    pagination:'.swiper-pagination',
+                    loop:true
+                },
+                swiperList:[{
+                    id:'001',
+                    imgUrl:'https://aecpm.alicdn.com/simba/img/TB1CWf9KpXXXXbuXpXXSutbFXXX.jpg_q50.jpg'
+                },{
+                    id:'0002',
+                    imgUrl:'https://aecpm.alicdn.com/simba/img/TB14ab1KpXXXXclXFXXSutbFXXX.jpg_q50.jpg'
+                }]
+            }
+        }
+
+    }
+</script>
+
+<style lang='stylus' scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+      background:#fff !important   
+  .wrapper
+      overflow: hidden
+      width :100%
+      height:0 
+      padding-bottom:31.25%
+      background: #eee;
+      .swiper-img
+        width:100% 
+      
+    
+</style>
